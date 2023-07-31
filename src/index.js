1acd043900b50d0bc498bc007d13731385d60534
Vladislav Chornyi <98426939+VladyslavIT@users.noreply.github.com>
2023-07-31 21:14:31 +0300

    fix markup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import {notes} from './utils/notes';
+import { notes } from './utils/notes';
+import { notesTitle } from './utils/notes';
+
 
 const noticeEl = document.querySelector('.notice');
+const barEl = document.querySelector('.bar-list');
 const noticeHTML = notes.map(noticeItemMarkup).join('');
+const barHTML = notesTitle.map(noticeBarMarkup).join('');
+barEl.insertAdjacentHTML('afterbegin', barHTML);
 noticeEl.insertAdjacentHTML('beforeend', noticeHTML);
 
+function noticeBarMarkup( title ) {
+  return `
+  <div class="bar-item">${title}</div>
+  `;
+}
 
-function noticeItemMarkup({id, name, createdAt, content, category, dates}) {
-    return ` <li class="notice-item" id=${id}>
+function noticeItemMarkup({ id, name, createdAt, content, category, dates }) {
+  return ` <li class="notice-item" id=${id}>
     <div class="notice-item-icon"></div>
         <p class="notice-item-name">${name}</p>
         <span class="notice-item-create">${createdAt}</span>
@@ -22,5 +32,5 @@ function noticeItemMarkup({id, name, createdAt, content, category, dates}) {
           <button class="button-archive">Арх</button>
           <button class="button-delete">Дел</button>
         </div>
-  </li>`
+  </li>`;
 }
